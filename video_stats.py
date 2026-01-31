d46e8f9e6bc1fec6b75fc55fe298cc9ca83bd7bb
Tako-C <phakphum2phrajren@gmail.com>
2026-01-31 12:38:05 +0700

    add function get_videos_in_playlist

--- video_stats.py
+++ video_stats.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
 
+max_results = 50
+
 
 CHANAL_HANDLE = "MrBeast"
 
@@ -29,5 +31,36 @@ def get_playlist_id():
 
     except requests.exceptions.RequestException as e:
         raise e
+    
+
+playlisId = get_playlist_id()
+
+def get_videos_in_playlist(playlist_id):
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={API_KEY}"
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an error for bad status codes
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+            
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
+                break
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
 if __name__ == "__main__":
-    get_playlist_id()
+    get_playlist_id() ## prints the playlist ID
+    print(get_videos_in_playlist(playlisId)) ## List of video IDs in the playlist
