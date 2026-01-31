3c723f218a563e03f80b8a87359b49cc89b1dd1e
Tako-C <phakphum2phrajren@gmail.com>
2026-01-31 13:36:32 +0700

    add extract_video_data &  save_to_json

--- video_stats.py
+++ video_stats.py
@@ -1,3 +1,4 @@
+from datetime import date
 import requests
 import json
 
@@ -31,11 +32,8 @@ def get_playlist_id():
 
     except requests.exceptions.RequestException as e:
         raise e
-    
 
-playlisId = get_playlist_id()
-
-def get_videos_in_playlist(playlist_id):
+def get_video_ids(playlist_id):
     video_ids = []
     pageToken = None
     base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={API_KEY}"
@@ -61,6 +59,48 @@ def get_videos_in_playlist(playlist_id):
     except requests.exceptions.RequestException as e:
         raise e
     
+def batch_list(video_id_list, batch_size):
+    for video_id in range(0,len(video_id_list), batch_size):
+        yield video_id_list[video_id:video_id + batch_size]
+
+
+def extract_video_data(video_id_list):
+    extracted_data = []
+    try:
+        for batch in batch_list(video_id_list,max_results):
+            video_id_list_str = ",".join(batch)
+            print(f"Processing batch: {video_id_list_str}")
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_id_list_str}&key={API_KEY}"
+            response = requests.get(url)
+            response.raise_for_status() 
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_data = {
+                    "video_id": item["id"],
+                    "title": item["snippet"].get("title"),
+                    "publishedAt": item["snippet"].get("publishedAt"),
+                    "duration": item["contentDetails"].get("duration"),
+                    "viewCount": item["statistics"].get("viewCount"),
+                    "likeCount": item["statistics"].get("likeCount"),
+                    "commentCount": item["statistics"].get("commentCount"),
+                }
+
+            extracted_data.append(video_data)
+        return extracted_data
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+    with open(file_path, "w" , encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4 ,ensure_ascii=False)
+
 if __name__ == "__main__":
-    get_playlist_id() ## prints the playlist ID
-    print(get_videos_in_playlist(playlisId)) ## List of video IDs in the playlist
+    playlisId = get_playlist_id()
+    video_ids = get_video_ids(playlisId)
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
+
+
